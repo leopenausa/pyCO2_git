f2a65131751ef330fab590cd66ecf96417bb5820
leopena <leopenausa@gmail.com>
2024-07-31 08:27:22 +0200

    Initial function definition

---
+++ random_generators.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def random_normal(mean, stdev, error):
+    """Computes random number from interval with normal gaussian probability given mean and 2SD stdev"""
+    res = 0
+    np.random.seed()
+
+    if error == "absolute":
+        res = np.random.normal(mean, stdev / 2)  # for 2sigma inputs
+        return res
+    elif error == "rsd":
+        res = np.random.normal(mean, mean * (stdev / 2))   # for 2sigma inputs
+        return res
+    
+def random_flat(mean, stdev, error):
+    """Computes random number from interval with flat distribution probability"""
+    res = 0
+    np.random.seed()
+
+    if error == "absolute":
+        res = np.random.uniform(mean - stdev / 2, mean + stdev / 2)    # for 2sigma inputs
+        return res
+    elif error == "rsd":
+        res = np.random.uniform(mean - (mean * stdev / 2), mean + (mean * stdev / 2))    # for 2sigma inputs
+        return res
